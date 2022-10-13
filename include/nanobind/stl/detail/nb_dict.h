b6e9ec4a5e6717dad7609a5daed6118c834dbaed
Matej Ferencevic <matej@omnisearch.ai>
2022-10-13 20:42:32 +0200

    Fix dict type caster compilation with `const &`

--- include/nanobind/stl/detail/nb_dict.h
+++ include/nanobind/stl/detail/nb_dict.h
@@ -54,9 +54,9 @@ template <typename Value_, typename Key, typename Element> struct dict_caster {
         if (ret) {
             for (auto &item : src) {
                 object k = steal(KeyCaster::from_cpp(
-                    forward_like<typename T::key_type>(item.first), policy, cleanup));
+                    forward_like<T>(item.first), policy, cleanup));
                 object e = steal(ElementCaster::from_cpp(
-                    forward_like<typename T::mapped_type>(item.second), policy, cleanup));
+                    forward_like<T>(item.second), policy, cleanup));
 
                 if (PyDict_SetItem(ret.ptr(), k.ptr(), e.ptr()) != 0) {
                     PyErr_Clear();

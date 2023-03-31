a7d0521d90b87105096b989e84a874871da4d9a6
Wenzel Jakob <wenzel.jakob@epfl.ch>
2023-03-31 17:19:17 +0200

    removed deprecated functionality

--- include/nanobind/tensor.h
+++
@@ -1,9 +0,0 @@
-#include "ndarray.h"
-
-#warning "The 'tensor.h' header and 'nanobind::tensor<..>' type are deprecated. Please use 'ndarray.h' and 'nanobind::ndarray<..>'"
-
-NAMESPACE_BEGIN(NB_NAMESPACE)
-
-template <typename... Args> using tensor = ndarray<Args...>;
-
-NAMESPACE_END(NB_NAMESPACE)

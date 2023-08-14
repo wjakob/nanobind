2d0ba349597ca05b55c47129f59f0df664a7b2fb
Wenzel Jakob <wenzel.jakob@epfl.ch>
2023-08-14 18:01:21 +0200

    mark failing PyPy/Darwin test as xfail

--- tests/common.py
+++ tests/common.py
@@ -3,6 +3,7 @@ import gc
 import pytest
 
 is_pypy = platform.python_implementation() == 'PyPy'
+is_darwin = platform.system() == 'Darwin'
 
 def collect():
     if is_pypy:
@@ -13,3 +14,6 @@ def collect():
 
 skip_on_pypy = pytest.mark.skipif(
     is_pypy, reason="This test currently fails/crashes PyPy")
+
+xfail_on_pypy_darwin = pytest.mark.xfail(
+    is_pypy and is_darwin, reason="This test for some reason fails on PyPy/Darwin")

2c1b72663adc59037a5b946c9c0cfb8d4d1f9a20
Nico Schlömer <nico.schloemer@gmail.com>
2023-09-19 10:11:21 +0200

    missing header

--- include/nanobind/nb_eval.h
+++ include/nanobind/nb_eval.h
@@ -1,6 +1,8 @@
 /*
-    pybind11/eval.h: Support for evaluating Python expressions and statements
-    from strings and files
+    nanobind/nb_eval.h: Support for evaluating Python expressions and statements
+                        from strings and files
+
+    Adapted from pybind11's eval.h with
 
     Copyright (c) 2016 Klemens Morgenstern <klemens.morgenstern@ed-chemnitz.de> and
                        Wenzel Jakob <wenzel.jakob@epfl.ch>
@@ -13,6 +15,7 @@
 
 #include <nanobind/nanobind.h>
 
+#include <string>
 #include <utility>
 
 NAMESPACE_BEGIN(NB_NAMESPACE)

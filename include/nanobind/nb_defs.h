a9e367c345a9b182ae2ea08c758abd290d364108
laggykiller <chaudominic2@gmail.com>
2024-03-01 19:46:14 +0800

    Remove unecessary check in nb_defs

--- include/nanobind/nb_defs.h
+++ include/nanobind/nb_defs.h
@@ -22,7 +22,7 @@
 #  define NAMESPACE_END(name) }
 #endif
 
-#if defined _WIN32 || defined __CYGWIN__
+#if defined(_WIN32)
 #  ifdef __GNUC__
 #    define NB_EXPORT        __attribute__ ((visibility("default")))
 #    define NB_IMPORT        NB_EXPORT

0161073ecddc96a9722f92a7dc1271cf7259589a
Rami Tabbara <rtabbara@gmail.com>
2024-07-24 04:31:32 +0200

    Handle print leaks when `inst_c2p` has collisions (#637)

--- src/nb_internals.cpp
+++ src/nb_internals.cpp
@@ -249,9 +249,19 @@ static void internals_cleanup() {
             fprintf(stderr, "nanobind: leaked %zu instances!\n",
                     internals->inst_c2p.size());
             #if !defined(Py_LIMITED_API)
-                for (auto [k, v]: internals->inst_c2p) {
+                auto print_leak = [](void* k, PyObject* v) {
                     PyTypeObject *tp = Py_TYPE(v);
                     fprintf(stderr, " - leaked instance %p of type \"%s\"\n", k, tp->tp_name);
+                };
+
+                for (auto [k, v]: internals->inst_c2p) {
+                    if (NB_UNLIKELY(nb_is_seq(v))) {
+                        nb_inst_seq* seq = nb_get_seq(v);
+                        for(; seq != nullptr; seq = seq->next)
+                            print_leak(k, seq->inst);
+                    } else {
+                        print_leak(k, (PyObject*)v);
+                    }
                 }
             #endif
         }

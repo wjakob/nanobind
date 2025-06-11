3bb057e38064ce1223a0e3e6c441a9a2d14807bc
Paul Caprioli <paul@hpkfft.com>
2025-06-11 09:42:11 -0700

    Add struct names implicit_t and enum_tbl_t

--- include/nanobind/nb_class.h
+++ include/nanobind/nb_class.h
@@ -94,6 +94,18 @@ enum class type_init_flags : uint32_t {
 // See internals.h
 struct nb_alias_chain;
 
+// Implicit conversions for C++ type bindings, used in type_data below
+struct implicit_t {
+    const std::type_info **cpp;
+    bool (**py)(PyTypeObject *, PyObject *, cleanup_list *) noexcept;
+};
+
+// Forward and reverse mappings for enumerations, used in type_data below
+struct enum_tbl_t {
+    void *fwd;
+    void *rev;
+};
+
 /// Information about a type that persists throughout its lifetime
 struct type_data {
     uint32_t size;
@@ -111,17 +123,8 @@ struct type_data {
     void (*copy)(void *, const void *);
     void (*move)(void *, void *) noexcept;
     union {
-        // Implicit conversions for C++ type bindings
-        struct {
-            const std::type_info **cpp;
-            bool (**py)(PyTypeObject *, PyObject *, cleanup_list *) noexcept;
-        } implicit;
-
-        // Forward and reverse mappings for enumerations
-        struct {
-            void *fwd;
-            void *rev;
-        } enum_tbl;
+        implicit_t implicit;  // for C++ type bindings
+        enum_tbl_t enum_tbl;  // for enumerations
     };
     void (*set_self_py)(void *, PyObject *) noexcept;
     bool (*keep_shared_from_this_alive)(PyObject *) noexcept;

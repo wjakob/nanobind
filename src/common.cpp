wjakob/nanobind

commit edf5753a13f98132b8da3d56fe94c31c678b2273

``seq_get_with_size()``: more aggressively reject incorrectly sized inputs

src/common.cpp
@@ -817,8 +817,14 @@ PyObject **seq_get_with_size(PyObject *seq, size_t size,
         }
 #  endif
     } else if (PySequence_Check(seq)) {
-        temp = PySequence_Tuple(seq);
+        Py_ssize_t size_seq = PySequence_Size(seq);
+        if (size_seq != (Py_ssize_t) size) {
+            if (size_seq == -1)
+                PyErr_Clear();
+            return nullptr;
+        }
 
+        temp = PySequence_Tuple(seq);
         if (temp)
             result = seq_get_with_size(temp, size, temp_out);
         else

commit dc35d69f65936280b2521941b2ce9d5ad16141d1

Fix regression in ``seq_get_with_size()``
Commit edf5753a13f98132b8da3d56fe94c31c678b2273 introduced a regression
in ``seq_get_with_size()`` where the ``temp_out`` argument was not
consistently initialized on all paths.

(Fortunately this did not slip into an official release yet!)

src/common.cpp
@@ -811,14 +811,13 @@ PyObject **seq_get_with_size(PyObject *seq, size_t size,
         if (size_seq != (Py_ssize_t) size) {
             if (size_seq == -1)
                 PyErr_Clear();
-            return nullptr;
+        } else {
+            temp = PySequence_Tuple(seq);
+            if (temp)
+                result = seq_get_with_size(temp, size, temp_out);
+            else
+                PyErr_Clear();
         }
-
-        temp = PySequence_Tuple(seq);
-        if (temp)
-            result = seq_get_with_size(temp, size, temp_out);
-        else
-            PyErr_Clear();
     }
 #else
     /* There isn't a nice way to get a PyObject** in Py_LIMITED_API. This
68550a860d419820562d48be752748cd86cce0c3
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-12-19 03:07:40 +0100

    Remove accidental duplicate entry

--- src/stubgen.py
+++ src/stubgen.py
@@ -94,7 +94,7 @@ SKIP_LIST = [
     # shouldn't appear in the stubs.
     "_new_member_", "_use_args_", "_member_names_", "_member_map_",
     "_value2member_map_", "_hashable_values_", "_unhashable_values_",
-    "_unhashable_values_", "_unhashable_values_map_", "_value_repr_",
+    "_unhashable_values_map_", "_value_repr_",
 ]
 
 # Interpreter-internal types.

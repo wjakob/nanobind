fe754ed565b648032246dacdb54f399f202e1117
Wenzel Jakob <wenzel.jakob@epfl.ch>
2026-01-02 12:00:03 +0100

    Make types immutable to benefit from adaptive specializing interpreter
    This commit improves binding performance using CPython's *adaptive
    specializing interpreter* ([PEP 659](https://peps.python.org/pep-0659))
    The speedups are automatic and require no changes to binding code:
    
    | Operation | Speedup | Requirements |
    |-----------|---------|--------------|
    | Method calls | **1.22x** faster | Python 3.11+ |
    | Static attribute lookups | **1.63x** faster | Python 3.14+ |
    
    This was achieved by making a number of nanobind-internal classes
    (``nb_func``, ``nb_method``, ``nb_meta``, etc.) immutable, which allows
    CPython to specialize generic ``LOAD_ATTR`` opcodes to faster type-specific
    versions (``LOAD_ATTR_METHOD`` for method calls, ``LOAD_ATTR_CLASS`` for
    static attribute lookups).

--- src/nb_abi.h
+++ src/nb_abi.h
@@ -14,7 +14,7 @@
 
 /// Tracks the version of nanobind's internal data structures
 #ifndef NB_INTERNALS_VERSION
-#  define NB_INTERNALS_VERSION 17
+#  define NB_INTERNALS_VERSION 18
 #endif
 
 #if defined(__MINGW32__)

2ebc0081672f688210f678f0f42eab721da01a2b
Patrick Day <patrickd@galil.com>
2026-04-24 16:13:32 -0700

    Add bitset caster

---
+++ include/nanobind/stl/bitset.h
@@ -0,0 +1,32 @@
+/*
+    nanobind/stl/bitset.h: type caster for std::bitset
+
+    All rights reserved. Use of this source code is governed by a
+    BSD-style license that can be found in the LICENSE file.
+*/
+
+#pragma once
+
+#include <nanobind/nanobind.h>
+#include <bitset>
+
+NAMESPACE_BEGIN(NB_NAMESPACE)
+NAMESPACE_BEGIN(detail)
+
+template <size_t T>
+struct type_caster<std::bitset<T>> {
+    NB_TYPE_CASTER(std::bitset<T>, const_name("int"))
+
+    bool from_python(handle src, uint8_t, cleanup_list *) noexcept {
+        value = std::bitset<T>(src.ptr());
+        return true;
+    }
+
+    static handle from_cpp(const std::bitset<T> &value, rv_policy,
+                           cleanup_list *) noexcept {
+        return cast(value.to_ullong());
+    }
+};
+
+NAMESPACE_END(detail)
+NAMESPACE_END(NB_NAMESPACE)

0c542e4380f9840c0a12b2db80fcdf4526bece19
Patrick Day <patrickd@galil.com>
2026-05-04 09:43:51 -0700

    Test 0 in each size of bitset, and add test case for when input cannot fit in bitset.

--- tests/test_stl.py
+++ tests/test_stl.py
@@ -885,13 +885,15 @@ def test75_weird_nones():
     assert t.takes_monostate(None) == "monostate"
 
 def test76_bitset():
-    assert t.bitset_1(0b0) == 0b0
-    assert t.bitset_1(0b1) == 0b1
-    assert t.bitset_8(0b1) == 0b1
+    assert t.bitset_1(0) == 0
+    assert t.bitset_1(1) == 1
+    with pytest.raises(TypeError, match="incompatible function arguments"):
+        t.bitset_1(2)
+    assert t.bitset_8(0) == 0
     assert t.bitset_8(0xFF) == 0xFF
-    assert t.bitset_64(0xFF) == 0xFF
+    assert t.bitset_64(0) == 0
     assert t.bitset_64((2**64) - 1) == (2**64) - 1
-    assert t.bitset_128(2**64) == 2**64
+    assert t.bitset_128(0) == 0
     assert t.bitset_128((2**128) - 1) == (2**128) - 1
     with pytest.raises(TypeError, match="incompatible function arguments"):
         t.bitset_1('test')
